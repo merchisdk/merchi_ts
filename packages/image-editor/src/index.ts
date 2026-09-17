1979f40143ff21f0b512ba8d9311f4d970d4a4fe
Cheng Yu <chengyu0316@gmail.com>
2026-09-18 09:39:07 +1000

    chore: integrate image editor workspace

---
+++ packages/image-editor/src/index.ts
@@ -0,0 +1,4 @@
+export { default } from './MerchiImageEditor';
+export { default as MerchiImageEditor } from './MerchiImageEditor';
+export { default as ImageEditor } from './ImageEditor';
+export { default as EditorImages } from './EditorImages';
